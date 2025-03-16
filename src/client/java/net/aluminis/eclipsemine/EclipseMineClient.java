d45ff039f9b562cf3c383aa95036208774db921a
Kaleb Gilson <kaleb.gilson@und.edu>
2025-03-16 18:51:52 -0500

    initial commit

---
+++ src/client/java/net/aluminis/eclipsemine/EclipseMineClient.java
@@ -0,0 +1,10 @@
+package client.java.net.aluminis.eclipsemine;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EclipseMineClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/aluminis/eclipsemine/EclipseMineDataGenerator.java
@@ -0,0 +1,11 @@
+package client.java.net.aluminis.eclipsemine;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EclipseMineDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
